045ea701e97cca0033964676f541bdf01548339f
LouisS <steimberg@hotmail.fr>
2017-03-21 10:01:23 +0100

    required new database objects

--- utils/require.php
+++ utils/require.php
@@ -1,9 +1,24 @@
 <?php
+
+
 require_once "lib/requireLibs.php";
 require_once "utils/randomHash.php";
 require_once "utils/devLogs.php";
 require_once "utils/DatabaseConnection.php";
 require_once  "utils/JsonUtils.php";
+
+// Models
+
+// User
+require_once "model/user/User.php";
+
+// Home
+require_once  "model/home/Home.php";
+
+// Building
+require_once  "model/buildings/Buildings.php";
+
+
 
 // Error Logging Facilities
 require_once "LoggerException.php";
