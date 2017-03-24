ab4aee95cd1a6a7e3da677c77e43e2fb278f6278
LouisS <steimberg@hotmail.fr>
2017-03-24 11:14:32 +0100

    deleted .idea

--- utils/DatabaseConnection.php
+++ utils/DatabaseConnection.php
@@ -24,6 +24,7 @@ class DatabaseConnection
         try {
             $this->db = new PDO($dbhost, $dbidentifier, $dbpw ,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $GLOBALS['salt'] = $config -> password_salt;
+            $this->createRepositories();
         }
         catch (Exception $e){
             require_once "view/connectionError.php";
@@ -35,5 +36,11 @@ class DatabaseConnection
     public function getDatabase(){
         return $this->db;
     }
+
+    private function createRepositories(){
+        $GLOBALS['repositories']['user'] = new UserRepository($this->db);
+    }
+
+
 
 }
