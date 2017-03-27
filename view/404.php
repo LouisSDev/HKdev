375bf502d233cace2ea91af015d0dae5ee7f8d27
Alexis Falempin <alexis.falempin@gmail.com>
2017-03-27 11:02:59 +0200

    MAJ error404

--- view/404.php
+++ view/404.php
@@ -3,14 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Erreur 404</title>
+    <link rel="stylesheet" type="text/css" href="/view/css/erro404.css">
 </head>
 <body>
-    <div class="error404">
-        <h1 class="page-title">Error 404</h1>
-    </div>
-    <div class="page-content">
-        <img src="/ressources/img/error404.jpg" alt="Ismael est noir">
-        <p>Oups ! Un problème est survenu ! Veuillez choisir une page existante !</p>
-    </div>
+    <section class="error">
+        <div class="error404">
+            <h1 class="page-title">
+                Error 404
+                Oups ! Un problème est survenu ! Veuillez choisir une page existante !
+            </h1>
+        </div>
+        <div class="page-content">
+            <img src="/ressources/img/error404.jpg" alt="Ismael est noir">
+            <p></p>
+        </div>
+    </section>
 </body>
 </html>
