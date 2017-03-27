80a394550318dff9852f3d70c48364cfc40e68f6
Alexis Falempin <alexis.falempin@gmail.com>
2017-03-27 10:44:52 +0200

    Config.json pb remote

--- view/404.php
+++ view/404.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Erreur 404</title>
 </head>
 <body>
-    <h1 > Erreur 404 </h1>
+    <div class="error404">
+        <h1 class="page-title">Error 404</h1>
+    </div>
+    <div class="page-content">
+        <img src="/ressources/img/error404.jpg" alt="Ismael est noir">
+        <p>Oups ! Un problème est survenu ! Veuillez choisir une page existante !</p>
+    </div>
 </body>
 </html>
