b51276febdf6325cf580130405f90f881747efc9
LouisS <steimberg@hotmail.fr>
2017-03-27 10:56:24 +0200

    User Repository creation

--- homepage.php
+++ homepage.php
@@ -3,7 +3,7 @@
 $dbConnector = new DatabaseConnection();
 $db = $dbConnector -> getDatabase();
 
-
+/*
 $user = new User();
 $saveUser = $user
     ->setAddress("35 rue du Bac")
@@ -27,3 +27,4 @@ if($saveUser == null){
         echo $user->getErrorMessage();
     }
 }
+*/
