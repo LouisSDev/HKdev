4afc7aaee77c796c1d906aaeafb531a33acc1fc8
Alexis Falempin <alexis.falempin@gmail.com>
2017-04-19 08:43:15 +0200

    CSS files update Global CSS edited

--- view/404.php
+++ view/404.php
@@ -3,15 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Erreur 404</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>\ressources\css\global.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>\ressources\css\error404.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 <body>
-    <div class="error404">
         <h1 class="page-title">Oups ! Un problème est survenu ! </h1>
-    </div>
     <div class="returnHP">
-        <button id="back_homepage">Retour à l'accueil</button>
+         <button class="btn">Retour à l'accueil</button>
     </div>
 </body>
 </html>
