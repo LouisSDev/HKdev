2240ec200d0c18dc2c079e7568864fdb82f98e43
Bocquet <aymericb76@hotmail.fr>
2017-04-19 11:43:05 +0200

    page d'accueil

---
+++ view/pageHome.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<include_once ("view/header.php");
+    <img src="img/photoaccueil.png">
+
+    <img src="img/entreprise.png">
+    <textarea>
+        Ces de ces deux constats qu’est né le projet HomeKeeper et c'est la raison pour laquelle nous mettons chaque jour toute notre énergie pour combattre ce fléau.
+        HomeKeeper, c’est le meilleur de la domotique jusqu’à chez vous, à bas prix, avec un SAV et une maintenance de qualité, pour moins consommer et mieux respecter notre chère planète Terre.
+        Non seulement vous réduirez votre consommation, mais vous améliorerez la sécurité et le confort de votre chez vous, tout en pouvant accéder aux données statistiques de la consommation de votre maison depuis n’importe quel appareil connecté (ordinateur, smartphone, tablette…).
+        Home Keeper est simple, ergonomique, pratique, économique et écologique, alors qu’attendez-vous?
+    </textarea>
+<include_once ("view/form.php");
+</body>
