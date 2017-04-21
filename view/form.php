541da5c9abaa8017ee89e6d6a297f5b0e6671064
Desazars <desazrom@hotmail.fr>
2017-04-21 09:18:26 +0200

    Home form

--- view/form.php
+++ view/form.php
@@ -11,31 +11,30 @@
 
     <form action="tag-html-balise-form" method="get" enctype="multipart/form-data">
         <p>
-            <input type="text" name="Name" value="Nom"/><br>
-            <input type="text" name="Prenom" value="Prénom"/><br>
+            <input type="text" name="First Name" value="Nom"/><br>
+            <input type="text" name="Last Name" value="Prénom"/><br>
             <input type="text" name="Adress" value="Adresse"/><br>
-            <input type="text" name="Code Postal" value="Code Postal"/><br>
             <input type="text" name="City" value="Ville"/><br>
             <input type="text" name="Mail" value="Adresse mail"/><br>
-            <input type="file" name="monfichier" /><br />
+            <input type="file" name="MyFile" /><br />
             <input class="btn" type="submit" value="Envoyer" />
         </p>
     </form>
     <?php
     // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
-    if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
+    if (isset($_FILES['MyFile']) AND $_FILES['MyFile']['error'] == 0)
     {
         // Testons si le fichier n'est pas trop gros
-        if ($_FILES['monfichier']['size'] <= 1000000)
+        if ($_FILES['MyFile']['size'] <= 1000000)
         {
             // Testons si l'extension est autorisée
-            $infosfichier = pathinfo($_FILES['monfichier']['name']);
+            $infosfichier = pathinfo($_FILES['MyFile']['name']);
             $extension_upload = $infosfichier['extension'];
             $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
             if (in_array($extension_upload, $extensions_autorisees))
             {
                 // On peut valider le fichier et le stocker définitivement
-                move_uploaded_file($_FILES['monfichier']['tmp_name'], 'uploads/' . basename($_FILES['monfichier']['name']));
+                move_uploaded_file($_FILES['MyFile']['tmp_name'], 'uploads/' . basename($_FILES['MyFile']['name']));
                 echo "L'envoi a bien été effectué !";
             }
         }
