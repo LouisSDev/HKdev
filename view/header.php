307d034e7758337b241200381f982e9bd76c0536
Bocquet <aymericb76@hotmail.fr>
2017-04-24 11:34:06 +0200

    DELETE "CONTACT" IN THE HEADER #ROMAIN WE DECIDE TO PLACE "CONTACT" IN THE FOOTER WITH THE OTHER !!!!!!!

--- view/header.php
+++ view/header.php
@@ -3,7 +3,6 @@
 <header class="header">
     <nav class="menu">
         <a href="<?php echo $GLOBALS['server_root']?>">Accueil</a>
-        <a href="<?php echo $GLOBALS['server_root']?>/contact">Contact</a>
         <a href="<?php echo $GLOBALS['server_root']?>/connexion">Connexion</a>
     </nav>
 </header>
