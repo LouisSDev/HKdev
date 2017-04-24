9e4aa0d0bfd9caf238ec30bec5397bae57ef9386
Alexis Falempin <alexis.falempin@gmail.com>
2017-04-24 11:49:31 +0200

    modified some files

---
+++ ressources/js/currentElementHeader.js
@@ -0,0 +1,15 @@
+var menu = $("a");
+console.log()
+
+
+/*var current = window.location.href;
+for(i=1;i<=$("a").length;i++){
+    var menu = $("a:nth-of-type(1)");
+    if(menu.attr("href") === current){
+        menu.attr("class", "current_element");
+    }
+    else if (menu.attr("href") !== current){
+        menu.removeAttr("class");
+    }
+}
+*/
