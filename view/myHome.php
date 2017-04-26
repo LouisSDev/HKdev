ce471a5f65f30ef9f00f4301c8bd12cef929c363
Alexis Falempin <alexis.falempin@gmail.com>
2017-04-26 17:01:19 +0200

    Page myHome created

---
+++ view/myHome.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ma maison</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/header.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/global.css">
+</head>
+<body>
+    <?php include_once("header.php");?>
+    <div class="home">
+        <a href=""><img src="<?php echo $GLOBALS['server_root']?>/ressources/img/home_icon.png"></a>
+    </div>
+    <div class="rooms">
+        <a href=""><img src="<?php echo $GLOBALS['server_root']?>/ressources/img/"></a>
+        <a href=""><img src="<?php echo $GLOBALS['server_root']?>/ressources/img/"></a>
+        <a href=""><img src="<?php echo $GLOBALS['server_root']?>/ressources/img/"></a>
+        <a href=""><img src="<?php echo $GLOBALS['server_root']?>/ressources/img/"></a>
+    </div>
+</body>
+</html>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexis Falempin
+ * Date: 26/04/2017
+ * Time: 16:07
+ */
