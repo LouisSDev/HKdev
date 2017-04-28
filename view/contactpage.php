9cb3c0f9d379ecf68ecddd8d429592cc39fc4ac5
Bocquet <aymericb76@hotmail.fr>
2017-04-28 12:00:39 +0200

    DELETE "CONTACT" IN THE HEADER #ROMAIN WE DECIDE TO PLACE "CONTACT" IN THE FOOTER WITH THE OTHER !!!!!!!

--- view/contactpage.php
+++ view/contactpage.php
@@ -2,22 +2,35 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/header.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/global.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/contactpage.css">
 </head>
 <body>
-<include_once ("view/header.php");
-<ins>
-Home Kepper
-    28, rue Notre-Dame des Champs
-    75006 Paris
-</ins>
-<form>
-    <input type="text" name="Nom" placeholder="Nom"></input>
-    <input type="text" name="Prénom" placeholder="Prénom"></input>
-    <input type="tel"  name="Téléphone" placeholder="Téléphone"></input>
-    <input type="text" name="Adresse mail" placeholder="Adresse mail"></input>
-    <input type="text" name="Sujet" placeholder="Sujet"></input>
-    <input type="text" name="Message" placeholder="Message"></input>
-</form>
+<?php include_once("header.php");?>
+<div class="address">
+    <p>
+        Home Kepper
+    </p>
+    <p>
+        28, rue Notre-Dame des Champs
+    </p>
+    <p>
+        75006 Paris
+    </p>
+</div>
+<div class="form">
+    <form action="tag-html-balise-form" method="get" enctype="multipart/form-data">
+        <input class=box type="text" name="First Name" placeholder="Nom"/><br>
+        <input class=box type="text" name="Last Name" placeholder="Prénom"/><br>
+        <input class=box type="text" name="tel" placeholder="Téléphone"/><br>
+        <input class=box type="text" name="Mail" placeholder="Adresse mail"/><br>
+        <input class=box type="text" name="Subject" placeholder="Sujet"/><br>
+        <input class=block type="textarea" name="Message" placeholder="Message"/><br>
+        <input type="file" name="MyFile" /><br />
+        <input class="btn" type="submit" value="Envoyer" />
+    </form>
+</div>
 </body>
 </html>
 
--- view/homepage.php
+++ view/homepage.php
@@ -6,11 +6,14 @@
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/homepage.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/header.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/global.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/connection.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/loader.js"></script>
     <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/smoothScrolling.js"></script>
+    <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/pop-up.js"></script>
 </head>
 <body>
+<div class="black"></div>
 <div class="loader">
     <h1>Please wait...</h1>
     <img src="<?php echo $GLOBALS['server_root']?>/ressources/img/gears.gif">
@@ -29,11 +32,11 @@
                 C’est pour atteindre cet idéal que nous travaillons chaque jour et que nous vous donnons la possibilité de profiter de notre savoir-faire et de nos innovations.
                 Depuis l'an 1000 nous oeuvrons à améliorer votre habitat. Nous avons commencé par inventer la maison, puis la sérure, avant d'inventer les fenetres et nous sommes les précurseurs d'une économie nouvelle.
                 Cette économie est nous l'avons appelée l'économie d'énerge.
-            <p>
+            </p>
             <p>
                 Avec une installation qui correspond à vos attentes et à votre habitat nous vous proposons aussi une maintenance efficace qui œuvre à ce que vous profitiez toujours des nouvelles technologies pour améliorer et sécuriser votre maison.
                 Jamais les logiciel de Home Keeper n'ont été piraté et nos utilisateurs économise chaque jour près de 2 000 Kw d'énergie.
-            <p>
+            </p>
             <p>
                 Home Keeper c’est aussi une quête pour l’environnement. Une mission qui nous tient à cœur pour améliorer nos conditions de vie sur Terre.
                 Choisissez Home Keeper pour Vous, choisissez Home Keeper pour Votre famille, choisissez Home Keeper pour votre Planète.
