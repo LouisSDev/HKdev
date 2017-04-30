87f3de2d5e229a28727b29d52001ff21cbf76513
maelstorm19 <ismael.goulani@yahoo.fr>
2017-04-30 23:47:26 +0200

    adding a method to edit email address

--- controller/UserController.php
+++ controller/UserController.php
@@ -3,13 +3,12 @@
 class UserController extends Controller
 {
     protected $connectionRequired = true;
+
     public function getDashboard()
     {
         $this->generateView('dashboard.php');
     }
-    /**
-     *
-     */
+
     public function modifyExistingPasswordAction()
     {
         /** @var User $user */
@@ -34,4 +33,51 @@ class UserController extends Controller
             $this->generateView('editProfile.php');
         }
     }
+
+    public function editEmailAddress(){
+        $user = $GLOBALS['view']['user'];
+        if (!empty($_POST['password']) && !empty($_POST['currentEmailAddress']) && !empty($_POST['confirmNewEmail'])){
+
+            $password = $_POST['password'];
+            $password_encrypt = sha1($password.$GLOBALS['salt']);
+            $currentEmailAddress = $_POST['currentEmailAddress'];
+            $confirmNewEmail = $_POST['confirmNewEmail'];
+            $newEmailAddress = $_POST['newEmailAdress'];
+
+            if ($password_encrypt !==$user->getPassword()){
+
+                $this->args['error'] = "Le mot de passe saisi est incorect";
+                $this->generateView('editProfile.php');
+            }
+            elseif ($currentEmailAddress !=$user->getEmail()){
+
+                $this->args['error'] = "L'adresse email rentré est érronée";
+                $this->generateView('editProfile.php');
+            }
+            elseif($newEmailAddress !== $currentEmailAddress){
+
+                    $this->args['error'] = "Les deux adresses email doivent etre identiques";
+                    $this->generateView('editProfile.php');
+            }
+            else{
+                if(preg_match('#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#' ,$newEmailAddress ) &&
+                    preg_match('#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#' , $confirmNewEmail ) ){
+                    $this->args['succes_message'] = "Félicitation votre email a bien été modifié";
+                    $this->generateView('editProfile.php');
+                }
+                else{
+                    $this->args['error'] = "Les adresses saisies ne sont pas des emails";
+                    $this->generateView('editProfile.php');
+                    $user-> save($this->db);
+                }
+            }
+
+            }
+            else{
+                $this->args['error'] = "Veuillez remplir les champs";
+                $this->generateView('editProfile.php');
+            }
+
+        }
+
 }
