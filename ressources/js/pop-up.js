3199fdae25e86a5026e3b7088dd0ecaa1c7cd482
Alexis Falempin <alexis.falempin@gmail.com>
2017-05-01 16:21:29 +0200

    corrected time loading popup and size

--- ressources/js/pop-up.js
+++ ressources/js/pop-up.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     $('#show').click(function () {
         $('.content').css('opacity', '0.4');
-        $('#modal').fadeIn(2000);
+        $('#modal').fadeIn(500);
     });
     $('.close').click(function () {
-        $('#modal').fadeOut(2000);
+        $('#modal').fadeOut(500);
         $('.content').css('opacity', '1');
     });
 });
