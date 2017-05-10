e0dfd3f90a62590b7d0d729ebf843adfb65ce0c7
Alexis Falempin <alexis.falempin@gmail.com>
2017-05-10 14:18:34 +0200

    Enhancement on selection menu element algorithm

--- ressources/js/currentElementHeader.js
+++ ressources/js/currentElementHeader.js
@@ -1,8 +1,10 @@
-var current = window.location.href;
-for(var i=1;i<=$("a").length;i++){
-    var menu = $("a:nth-of-type("+i+")");
-    if(menu.attr("href") === current){
-        menu.css('color', '#62bcda');
-        break;
+$(document).ready(function () {
+    var current = window.location.href;
+    for(var i=0;i<$("nav > a").length;i++){
+        var menu = $("nav > a")[i];
+        if($(menu).attr("href") === current){
+            $(menu).css('color', '#62bcda');
+            break;
+        }
     }
-}
+});
