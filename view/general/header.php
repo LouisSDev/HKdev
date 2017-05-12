5e1e0035b3d72e8c1ab132abe435107baaec5341
Alexis Falempin <alexis.falempin@gmail.com>
2017-05-12 16:21:22 +0200

    Header corrected to avoid 404 on some links

--- view/general/header.php
+++ view/general/header.php
@@ -3,9 +3,9 @@
         if(isset($GLOBALS['view']['connected'])){
            ?>
             <nav class="menu">
-                <a href="<?php echo $GLOBALS['server_root'] . '/user/home/general'// TODO : not the right path, please check the rooting.php or ask to the AWESOME Louis (also known as "THE VERY BEST", "King of the universe", "the Motherfucker") for more information. Also add other described  pages in this header ?>">Ma maison</a>
+                <a href="<?php echo $GLOBALS['server_root'] . '/user/home'// TODO : not the right path, please check the rooting.php or ask to the AWESOME Louis (also known as "THE VERY BEST", "King of the universe", "the Motherfucker") for more information. Also add other described  pages in this header ?>">Ma maison</a>
                 <a href="<?php echo $GLOBALS['server_root'] . '/user/dashboard'?>">Tableau de bord</a>
-                <a href="<?php echo $GLOBALS['server_root'] . '/user/editProfile'?>">Mon compte</a>
+                <a href="<?php echo $GLOBALS['server_root'] . '/user/edit'?>">Mon compte</a>
                 <a href="<?php echo $GLOBALS['server_root'] . '/contact'?>">Contact</a>
                 <a href="<?php echo $GLOBALS['server_root'] . '/user/disconnect'?>">Déconnexion</a>
             </nav>
@@ -14,8 +14,8 @@
         else{
      ?>
     <nav class="menu">
-        <a href="<?php echo $GLOBALS['server_root']?>/">Accueil</a>
-        <a href="<?php echo $GLOBALS['server_root'] . '/contact/'?>">Contact</a>
+        <a href="<?php echo $GLOBALS['server_root']?>">Accueil</a>
+        <a href="<?php echo $GLOBALS['server_root'] . '/contact'?>">Contact</a>
         <a id="show" href="#">Connexion</a>
     </nav>
     <?php
