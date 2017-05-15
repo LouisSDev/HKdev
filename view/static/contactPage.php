67f182e6c1865c11cf9bc406b89b03961d9c1ed5
Bocquet <aymericb76@hotmail.fr>
2017-05-15 10:35:50 +0200

    test contactPage Map responsive

--- view/static/contactPage.php
+++ view/static/contactPage.php
@@ -11,6 +11,7 @@
     <script src="https://use.fontawesome.com/86ed160d29.js"></script>
     <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/googleMap.js"></script>
     <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/pop-up.js"></script>
+    <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/smoothScrolling.js"></script>
 
 
 </head>
@@ -18,30 +19,42 @@
     <?php include_once ($GLOBALS['root_dir'] . "/view/general/modal.php");?>
     <div class="content">
         <?php include_once($GLOBALS['root_dir'] . "/view/general/header.php");?>
-        <div class="address">
-            <p>
-                Home Kepper
-            </p>
-            <p>
-                28, rue Notre-Dame des Champs
-            </p>
-            <p>
-                75006 Paris
-            </p>
+        <div id="textMap">
+            <div class="grid">
+                <div class="column1">
+                    <p>
+                        Home Kepper
+                    </p>
+                    <p>
+                        28, rue Notre-Dame des Champs
+                    </p>
+                    <p>
+                        75006 Paris
+                    </p>
+                </div>
+                <div class="column2">
+                    <div id="googleMap" style="width:30%;height:300px;"></div>
+                    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfUVXK1gT4aQSnm4RZPoFHjc0MU-8U1vw&callback=myMap"></script>
+                </div>
+            </div>
+            <div class="form_pointer">
+                <a class="arrow_black scrolling" href="#contactForm">&raquo;</a>
+            </div>
         </div>
-        <div id="googleMap" style="width:100%;height:400px;"></div>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfUVXK1gT4aQSnm4RZPoFHjc0MU-8U1vw&callback=myMap"></script>
-        <div class="form contactForm">
-            <form>
-                <fieldset>
+        <div id="contactForm">
+            <p class="textForm">
+                Des questions ?
+            </p>
+            <div class="form contactForm">
+                <form>
                     <input type="text" name="field1" placeholder="Nom ">
                     <input type="text" name="field2" placeholder="Prénom">
                     <input type="email" name="field3" placeholder="Adresse mail">
                     <input type="text" name="field4" placeholder="Sujet">
                     <textarea class="message" name="field5" placeholder="Message"></textarea>
-                </fieldset>
-                <input class="button" type="submit" value="Envoyer" />
-            </form>
+                    <input class="button" type="submit" value="Envoyer" />
+                </form>
+            </div>
         </div>
     </div>
 </body>
