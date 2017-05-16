4630bfbda7acdbe64fd0e518b772f464a324ea99
Adrien <adrien.arnal@isep.fr>
2017-05-16 10:17:41 +0200

    css error

--- view/general/error.php
+++ view/general/error.php
@@ -1,26 +1,29 @@
-<?php
+<link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/global.css">
+<div class="error">
+    <?php
 
-if(isset ($GLOBALS['view']['success_message'])){ ?>
-    <div class="success-message">
-        <?php echo $GLOBALS['view']['success_message'] ?>
-    </div>
-    <?php
-}
-elseif( isset($GLOBALS['view']['error_message'])) { ?>
-    <div class="error-message">
-        <?php
-            echo $GLOBALS['view']['error_message']  ;
-            if( isset($GLOBALS['view']['errors'])) {
-        ?>
-        <div class="error-details">
-             <?php
-                foreach ($GLOBALS['view']['errors'] as $err){
-                    echo '<p>' . $err . '</p></br>';
-                }
-             ?>
-        </div>
-        <?php }?>
-    </div>
-    <?php
-}
-?>
+        if(isset ($GLOBALS['view']['success_message'])){ ?>
+            <div class="success-message">
+                <?php echo $GLOBALS['view']['success_message'] ?>
+            </div>
+            <?php
+        }
+        elseif( isset($GLOBALS['view']['error_message'])) { ?>
+            <div class="error-message">
+                <?php
+                    echo $GLOBALS['view']['error_message']  ;
+                    if( isset($GLOBALS['view']['errors'])) {
+                ?>
+                <div class="error-details">
+                     <?php
+                        foreach ($GLOBALS['view']['errors'] as $err){
+                            echo '<p>' . $err . '</p></br>';
+                        }
+                     ?>
+                </div>
+                <?php }?>
+            </div>
+            <?php
+        }
+    ?>
+</div>
