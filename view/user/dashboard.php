921e696df3714f7d53710646c45a188a7a41a52b
LouisS <steimberg@hotmail.fr>
2017-05-17 00:30:35 +0200

    Adding Chart.js implementation

--- view/user/dashboard.php
+++ view/user/dashboard.php
@@ -4,11 +4,37 @@
     <title>Editer mon profil</title>
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/global.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/header.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://use.fontawesome.com/86ed160d29.js"></script>
-    <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/editProfile.js"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/modal.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+    <script src="https://use.fontawesome.com/86ed160d29.js"></script>
+    <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/chart/chart.js"></script>
     <script src="<?php echo $GLOBALS['server_root']?>/ressources/js/pop-up.js"></script>
+
+    <style>
+
+        .bar{
+            fill: steelblue;
+        }
+
+        .bar:hover{
+            fill: brown;
+        }
+
+        .axis {
+            font: 10px sans-serif;
+        }
+
+        .axis path,
+        .axis line {
+            fill: none;
+            stroke: #000;
+            shape-rendering: crispEdges;
+        }
+
+    </style>
+
 </head>
 <body>
 <?php
@@ -16,6 +42,9 @@
 $user = $GLOBALS['view']['user'] ;
 include_once($GLOBALS['root_dir'] . '/view/general/header.php') ?>
 <h1>TABLEAU DE BORD</h1>
-<h3>Bonjour <?php echo $user -> getFirstName()?></h3>
+    <h3>Bonjour <?php echo $user -> getFirstName()?></h3>
+
+    <svg width="960" height="500"></svg>
+    <div class="test"></div>
 </body>
 </html>
