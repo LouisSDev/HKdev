388cc7d35d3d498c9a162ee5c30e3636839bf168
maelstorm19 <ismael.goulani@yahoo.fr>
2017-05-16 13:42:40 +0200

    adminBuilding css v1

--- controller/AccountManagingController.php
+++ controller/AccountManagingController.php
@@ -1,27 +1,19 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: LOUISSTEIMBERG
  * Date: 12/05/2017
  * Time: 09:52
  */
-
 abstract class AccountManagingController extends Controller
 {
-
     protected $connectionRequired = true;
-
-
-
-
     /**
      * @param Home $building
      * @param $homeId
      * @return Home
      */
     protected function findHomeFromIdInBuilding(Home $building, $homeId){
-
         $home = null;
         /**
          * @var Home $building
@@ -31,39 +23,27 @@ abstract class AccountManagingController extends Controller
              * @var Home $hm
              */
             foreach ($building -> getHomes() as $hm){
-
                 if ($hm -> getId() === $homeId){
                     $home = $hm;
                     break;
                 }
-
             }
-
         }
-
         if($home)
         {
             return $home;
         }
-
         $this -> apiResponse = new ApiResponse(404, 'L\'habitation spécifiée n\'est pas disponible', true);
-
         $this -> generateView('static/404.php', '404');
         exit();
-
     }
-
     /**
      * @param $id
      * @return Home
      */
-
     protected function findHomeFromId($id, $onlyAdmin = false){
-
-
         /** @var Home $home */
         $home = null;
-
         /** @var Home $hm */
         foreach ($this -> user -> getHomes() as $hm)
         {
@@ -74,19 +54,14 @@ abstract class AccountManagingController extends Controller
                 break;
             }
         }
-
         if($home)
         {
             return $home;
         }
-
         $this -> apiResponse = new ApiResponse(404, 'L\'habitation spécifiée n\'est pas disponible', true);
-
         $this -> generateView('static/404.php', '404');
         exit();
-
     }
-
     /**
      * @param Home $home
      * @param $room
@@ -97,25 +72,19 @@ abstract class AccountManagingController extends Controller
         $room = null;
         /** @var Room $rm */
         foreach($home -> getRooms() as $rm ){
-
             if($rm -> getId() === $roomId){
                 $room = $rm;
                 break;
             }
         }
-
         if($room){
             return $room;
         }
-
         $this -> apiResponse = new ApiResponse(404, 'La pièce spécifiée n\'est pas disponible', true);
-
         $this -> generateView('static/404.php', '404');
         exit();
     }
-
     protected function findRoomFromIdInUsersRooms($roomId){
-
         $room = null;
         /** @var Room $rm */
         foreach($this -> user -> getAllRooms() as $rm ){
@@ -124,18 +93,13 @@ abstract class AccountManagingController extends Controller
                 break;
             }
         }
-
         if($room){
             return $room;
         }
-
         $this -> apiResponse = new ApiResponse(404, 'La pièce spécifiée n\'est pas disponible', true);
-
         $this -> generateView('static/404.php', '404');
         exit();
-
     }
-
     /**
      * @param $id
      * @param Home $home
@@ -151,34 +115,26 @@ abstract class AccountManagingController extends Controller
                 break;
             }
         }
-
         if($sensor){
             return $sensor;
         }
-
         $this -> apiResponse = new ApiResponse(404, 'Le capteur spécifié n\'est pas disponible', true);
-
         $this -> generateView('static/404.php', '404');
         exit();
     }
-
     protected function updateEffectors($effectors){
         /** @var Effector $eff */
         foreach ($effectors as $eff){
-
             if($eff -> getEffectorType() -> getType() === $_POST['effectorType']){
                 $effectors[] = $eff;
-
                 if($eff -> getEffectorType() -> getChart() && !empty($_POST['state'])){
                     ApiHandler::throwError(400, 'Vous ne pouvez pas modifier l\'état de ce type d\'effecteur');
                 }
-
                 if(!$eff -> getEffectorType() -> getChart() && !empty(empty($_POST['value']))){
                     ApiHandler::throwError(400, 'Vous ne pouvez pas modifier la valeur de ce type d\'effecteur');
                 }
             }
         }
-
         /** @var Effector $eff */
         foreach($effectors as $eff){
             if($eff -> getEffectorType() -> getChart()){
@@ -189,7 +145,6 @@ abstract class AccountManagingController extends Controller
                     $eff -> setAuto($_POST['auto']);
                 }
             }
-
             else{
                 if(!empty($_POST['state'])){
                     $eff -> setState($_POST['state']);
@@ -198,14 +153,8 @@ abstract class AccountManagingController extends Controller
                     $eff -> setAuto($_POST['auto']);
                 }
             }
-
         }
-
         $this -> user -> save($this -> db);
-
         ApiHandler::returnValidResponse(null);
     }
-
-
-
 }
--- controller/BuildingController.php
+++ controller/BuildingController.php
@@ -33,16 +33,12 @@ class BuildingController extends AccountManagingController
 
 
                 if ($this -> user -> getPassword() === sha1($_POST['adminPassword'] . $GLOBALS['salt'])) {
-                    if ($_POST['password'] === $_POST['passwordConf']) {
-                        $toBeEditedUser = $this
-                            ->findHomeFromIdInBuilding($this->args['building'], $_POST['homeId'])
-                            ->getUser();
 
-                        $this->setNewUserParams($toBeEditedUser);
-                    }
-                    else{
-                        $this -> args['error_message'] = "Les deux nouveaux mot de passes ne sont pas identiques";
-                    }
+                    $toBeEditedUser = $this
+                        ->findHomeFromIdInBuilding($this->args['building'], $_POST['homeId'])
+                        -> getUser();
+
+                    $this -> setNewUserParams($toBeEditedUser);
                 }
                 else{
                     $this -> args['error_message'] = "Le mot de passe entré n'est pas correct";
