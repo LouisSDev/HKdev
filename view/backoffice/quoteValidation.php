8ce9d21f57bf6a81d11b3b7ddd978fdd5c658c12
Desazars <desazrom@hotmail.fr>
2017-05-19 09:11:56 +0200

    quoteValidation

---
+++ view/backoffice/quoteValidation.php
@@ -0,0 +1,54 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Gérer les Capteurs et les Effecteurs</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/quoteValidation.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/header.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['server_root']?>/ressources/css/global.css">
+    <?php include_once ($GLOBALS['root_dir'] . "/view/general/adminHeader.php");?>
+</head>
+
+<body>
+<br>
+<?php
+
+$user = $GLOBALS['view']['user'];
+
+include_once($GLOBALS['root_dir'] . '/view/general/error.php');
+
+?>
+<h1>Devis à valider </h1>
+<div class="aValider">
+    <div class="valider">
+        <?php
+        foreach (SensorType::TYPE_ARRAY as $type){
+
+            echo '<optgroup label="'. $type . '">';
+
+            /** @var  $sensorType SensorType*/
+            foreach ($sensorsTypes as $sensorType) {
+
+                if ($sensorType->getType() === $type && $sensorType -> getSelling()) {
+                    echo '<option label="" value="'
+                        . $sensorType -> getId() . '">'
+                        . $type . ' : ' . $sensorType -> getName()
+                        . ' - ' . $sensorType -> getRef()
+                        . ' : ' . $sensorType -> getPrice() . '€</option>';
+                }
+            }
+            echo '</optgroup>';
+
+        }
+        ?>
+
+            <input class="btn" type="submit" value="Valider" />
+
+    </div>
+</div>
+
+<div class="tableauAdd">
+
+</div>
+
+
+</body>
