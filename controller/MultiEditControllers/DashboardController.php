7ca6e9f552dc7a327061e03603ecf3a0444f3198
boubasse <alexis.falempin@gmail.com>
2017-05-21 01:18:12 +0200

    Reverting to previous state to avoid bugs ^^

--- controller/MultiEditControllers/DashboardController.php
+++ controller/MultiEditControllers/DashboardController.php
@@ -9,37 +9,7 @@
 class DashboardController extends AdminController
 {
 
-    public function getEffectorsStocksByType(){
-        $effectorTypeRepository = $this -> getEffectorTypeRepository();
-        $effectorTypes = $effectorTypeRepository -> getAll();
-        $effectorsByType = array();
-        /** @var EffectorRepository $effectorRepo */
-        /** @var EffectorType $effectorType */
-        foreach ($effectorTypes  as $effectorType -> getName() => $effectorRepo -> getUnusedEffectorsByType($effectorType -> getId())){
-            $effectorsByType[$effectorType -> getName()] = $effectorRepo -> getUnusedEffectorsByType($effectorType -> getId());
-        }
-        return $effectorsByType;
-    }
 
-    public function getSensorsStocksByType(){
-        $sensorTypeRepo = $this -> getSensorRepository();
-        $sensorTypes = $sensorTypeRepo -> getAll();
-        $sensorsByType = array();
-        /** @var SensorRepository $sensorRepo */
-        /** @var SensorType $sensorType */
-        foreach ($sensorTypes as $sensorType -> getName() => $sensorTypeRepo -> getSensorsUnusedByType($sensorType -> getId())){
-            $sensorsByType[$sensorType -> getName()] = $sensorTypeRepo -> getSensorsUnusedByType($sensorType -> getId());
-        }
-        return $sensorsByType;
-    }
-
-    public function createStocksArrays(){
-        $effectorStock = $this->getEffectorsStocksByType();
-        $sensorStock = $this->getSensorsStocksByType();
-        $this->args['effectorStock'] = json_encode($effectorStock);
-        $this->args['sensorStock'] = json_encode($sensorStock);
-        $this -> generateView('backoffice/dashboard.php',"Tableau de bord Administrateur");
-    }
 
 
 
