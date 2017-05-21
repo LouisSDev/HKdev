8431963754f51bb9ec87aeef27b869edb4628d66
Desazars <desazrom@hotmail.fr>
2017-05-21 21:28:42 +0200

    effectors

--- controller/MultiEditControllers/AdminLoggingsFormController.php
+++ controller/MultiEditControllers/AdminLoggingsFormController.php
@@ -8,5 +8,37 @@
  */
 class AdminLoggingsFormController extends DashboardController
 {
+    private function removeEffector($effectorsTypes)
+    {
 
+        if (!empty($_POST['effectorType'])) {
+            /** @var EffectorType $effectorType */
+            $effectorType = null;
+
+            /**@var EffectorType $stp */
+            foreach ($effectorsTypes as $stp) {
+                if ($stp->getId() === $_POST['effectorType']) {
+                    $effectorType = $stp;
+                    break;
+                }
+            }
+
+            if ($effectorType) {
+
+                $effectorType->setSelling(false);
+
+                if ($effectorType->save($this->db)) {
+
+                    $this->args['success_message'] = "Félicitation l'effecteur sélectionné a bien été supprimé";
+                } else {
+                    $this->args['error_message'] = "Les données entrées ne sont pas valides";
+                    $this->args['errors'] = $effectorType->getErrorMessage();
+                }
+            } else {
+                $this->args['error_message'] = "Les données entrées ne sont pas valides";
+            }
+        }else{
+            $this->args['error_message'] = "Veuillez sélectionner un effecteur";
+        }
+    }
 }
