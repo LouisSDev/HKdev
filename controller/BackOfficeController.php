ba39eb142369ae2ad09edd3dcc412235255fb0fb
Bocquet <aymericb76@hotmail.fr>
2017-05-22 10:06:16 +0200

    change and method modification

--- controller/BackOfficeController.php
+++ controller/BackOfficeController.php
@@ -430,6 +430,61 @@ class BackOfficeController extends AdminStaticController
         $this->generateView('backoffice/dashboard.php', "Tableau de bord Administrateur");
     }
 
+    protected function addEffectorType(){
+        $effectorType = new EffectorType();
+        $effectorType ->createFromResults($_POST);
+
+        if($effectorType-> save($this->db)){
+            $this->args['success_message'] = "Félicitation l'effecteur sélectionné a bien été ajouté";
+        } else {
+            $this->args['error_message'] = "Les données entrées ne sont pas valides";
+        }
+
+    }
+
+
+
+
+
+    protected function addEffectors($effectorTypes){
+
+        if (!empty($_POST['effectorTypeId']) && !empty($_POST['effectorNb'])){
+
+            /**@var EffectorType $effectorType*/
+            $effectorType = null;
+
+            /**@var EffectorType $etp*/
+            foreach ($effectorTypes as $etp) {
+                Utils::addWarning($etp->getId()  . '   ' . $_POST['effectorTypeId'] );
+                if ($etp->getId() == $_POST['effectorTypeId']) {
+                    $effectorType = $etp;
+                }
+            }
+
+            if($effectorType){
+
+                for($i = 1 ; $i <= $_POST['effectorNb'] ; $i++){
+                    $effector = new Effector();
+                    if ($effector->setEffectorType($effectorType)->save($this->db) ) {
+                        $this->args['success_message'] = "Félicitation les effecteurs ont bien été ajoutés aux stocks informatiques";
+                    }
+                    else {
+                        $this->args['error_message'] = "Les données entrées ne sont pas valides";
+                        $this->args['errors'] = $effectorType->getErrorMessage();
+                    }
+                }
+            }
+            else{
+                $this->args['error_message'] = "Les données entrées ne sont pas valides";
+            }
+        }
+        else{
+            $this->args['error_message'] = "Veuillez sélectionner un effecteur";
+
+        }
+
+    }
+
 
 
 }
