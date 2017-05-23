8e5081e982d0371d4cfe44b7ac55c7612392b1e0
Bocquet <aymericb76@hotmail.fr>
2017-05-23 20:45:09 +0200

    remove room done

--- controller/MultiEditControllers/AdminStaticController.php
+++ controller/MultiEditControllers/AdminStaticController.php
@@ -10,48 +10,38 @@ class AdminStaticController extends AdminLoggingsFormController
 {
     protected function removeRoom($rooms)
     {
-
         if (!empty($_POST['room'])) {
             /** @var Room $room */
             $room = null;
-
             /**@var Room $rm */
-
             foreach ($rooms as $rm) {
                 if ($rm->getId() === $_POST['roomId']) {
                     $room = $rm;
                     break;
                 }
             }
-
             if ($room) {
-
-                /**@var Effector $eff */
-
-
-                foreach ($room->getEffectors() as $eff){
-
-                    $eff->delete($this->db);
-
-                }
-
-                /**@var Sensor $sns */
-
-                foreach ($room->getSensors() as $sns){
-                    $this->getSensorValueRepository()->deleteValuesFromSensors($sns->getId());
-                    $sns->delete($this->db);
-                }
-
-                $room->delete($this->db);
-
+                $this -> deleteRoom($room);
                 $this->args['success_message'] = "Félicitation vous avez bien supprimé une pièce";
-
             } else {
                 $this->args['error_message'] = "Les données entrées ne sont pas valides";
             }
         }else{
             $this->args['error_message'] = "Veuillez sélectionner une pièce à supprimer";
         }
+    }
+
+    protected function deleteRoom(Room $room){
+        /**@var Effector $eff */
+        foreach ($room->getEffectors() as $eff){
+            $eff->delete($this->db);
+        }
+        /**@var Sensor $sns */
+        foreach ($room->getSensors() as $sns){
+            $this->getSensorValueRepository()->deleteValuesFromSensors($sns->getId());
+            $sns->delete($this->db);
+        }
+        $room->delete($this->db);
     }
 
 }
