acb2bbe476f8f68305b07ccb6591b42ae218d948
Desazars <desazrom@hotmail.fr>
2017-05-26 09:34:50 +0200

    manage users

--- controller/UserGestionController.php
+++ controller/UserGestionController.php
@@ -145,7 +145,7 @@ class UserGestionController  extends AdminStaticController
 
                 if ($homeUser) {
                     $this -> deleteHome($homeUser);
-                  $this->args['success_message'] = "Félicitation le capteur sélectionné a bien été supprimé";
+                  $this->args['success_message'] = "Félicitation la maison sélectionné a bien été supprimé";
                 } else {
                   $this->args['error_message'] = "Les données entrées ne sont pas valides";
                 }
@@ -191,6 +191,16 @@ class UserGestionController  extends AdminStaticController
 
             if ($deletedUser) {
 
+                /** @var Home $home */
+                foreach ($deletedUser -> getHomes() as $home) {
+                    if($home -> getHasHomes()){
+                        /** @var Home $hm */
+                        foreach($home as $hm){
+                            $hm -> delete($this -> db);
+                        }
+                    }
+                }
+
                 /**@var Home $hm */
                 foreach ($deletedUser->getHomes() as $hm){
                     $this -> removeHome($hm);
@@ -198,7 +208,7 @@ class UserGestionController  extends AdminStaticController
 
                 $deletedUser->delete($this->db);
 
-                $this->args['success_message'] = "Félicitation le capteur sélectionné a bien été supprimé";
+                $this->args['success_message'] = "Félicitation l'utilisateur sélectionné a bien été supprimé";
             } else {
                 $this->args['error_message'] = "Les données entrées ne sont pas valides";
             }
