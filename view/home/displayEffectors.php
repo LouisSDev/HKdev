9f500e688997c59398caf80280ce325ee9556f54
Alexis Falempin <afalempin@juniorisep.com>
2017-06-06 15:01:00 +0200

    Corrected problem in saving user password Changes in CSS of MyHome

--- view/home/displayEffectors.php
+++ view/home/displayEffectors.php
@@ -8,7 +8,7 @@ function displayEffectors(Room $room){
             echo '<div class="effectors-input temp effectors-' . $room -> getId() . '">
                     <input class="value" type="number" step="1" value="15" min="0" max="25">
                     <p class="info">Température</p>
-                    <button id="temp-effector-' . $room -> getId() . '" roomId="' . $room -> getId() . '" class ="save-temp-effector">
+                    <button id="temp-effector-' . $room -> getId() . '" roomId="' . $room -> getId() . '" class ="btn save-temp-effector">
                         Sauvegarder
                     </button>    
                 </div>';
@@ -18,7 +18,7 @@ function displayEffectors(Room $room){
                     <i class="fa fa-toggle-off switch-off" roomId="' . $room -> getId() . '" id="switch-off-' . $room -> getId() . '" aria-hidden="true" style="cursor:pointer;"></i>
                     <i class="fa fa-toggle-on switch-on" roomId="' . $room -> getId() . '" id="switch-on-' . $room -> getId() . '"  aria-hidden="true" style="cursor:pointer;"></i>
                     <p class="info">Volets automatique</p>
-                    <button id="volet-effector-' . $room -> getId() . '" roomId="' . $room -> getId() . '" class ="save-volet-effector">
+                    <button id="volet-effector-' . $room -> getId() . '" roomId="' . $room -> getId() . '" class ="btn save-volet-effector">
                         Sauvegarder
                     </button>    
                 </div>';
@@ -28,7 +28,7 @@ function displayEffectors(Room $room){
             echo '<div class="effectors-input lum effectors-' . $room -> getId() . '">
                     <input type="range"  min="0" max="100" id="lum-eff-val-' . $room -> getId() . '" />
                     <p class="info">Luminosité</p>
-                    <button id="lum-effector-' . $room -> getId() . '" roomId="' . $room -> getId() . '" class ="save-lum-effector">
+                    <button id="lum-effector-' . $room -> getId() . '" roomId="' . $room -> getId() . '" class ="btn save-lum-effector">
                         Sauvegarder
                     </button>    
                 </div>';
