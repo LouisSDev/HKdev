3d75541ebba46d0630f33fca293fd4a8c5d60fe1
Bocquet <aymericb76@hotmail.fr>
2017-06-12 11:16:32 +0200

    delete function myMap on general.js

--- ressources/js/general.js
+++ ressources/js/general.js
@@ -171,14 +171,3 @@ $(document).ready(function () {
     }
 
 });
-
-
-
-function myMap() {
-    var center= new google.maps.LatLng(48.845416, 2.328119);
-    var mapCanvas = document.getElementById("googleMap");
-    var mapOptions = {center: center, zoom: 17};
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({position: center});
-    marker.setMap(map);
-}
